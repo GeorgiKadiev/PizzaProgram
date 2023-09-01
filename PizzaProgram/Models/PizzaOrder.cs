a805a75f3cb9360554e35696b02579a3e87dedfa
GeorgiKadiev <georgi.k.kadiev@gmail.com>
2023-09-01 23:01:19 +0300

    initial commit

---
+++ PizzaProgram/Models/PizzaOrder.cs
@@ -0,0 +1,9 @@
+﻿namespace PizzaProgram.Models
+{
+    public class PizzaOrder
+    {
+        public int ID { get; set; }
+        public string PizzaName { get; set; }
+        public float BasePrice { get; set; }
+    }
+}
